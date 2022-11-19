2ac48d3a66f8fa66b74e9881f5df423b84679433
Nitinjiwnani <jiwnani01@gmail.com>
2022-11-19 18:03:10 +0530

    toast integration

--- lib/utils/utils.dart
+++ lib/utils/utils.dart
@@ -1,1 +1,12 @@
-class Utils {}
+import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
+
+class Utils {
+  static toastmessage(String message) {
+    Fluttertoast.showToast(
+        msg: message,
+        backgroundColor: Colors.black,
+        textColor: Colors.white,
+        toastLength: Toast.LENGTH_SHORT);
+  }
+}
--- lib/view/login_screen.dart
+++ lib/view/login_screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/container.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter_mvvm_architecture/utils/routes/routes_name.dart';
+import 'package:flutter_mvvm_architecture/utils/utils.dart';
 import 'package:flutter_mvvm_architecture/view/home_screen.dart';
 
 class LoginScreen extends StatefulWidget {
@@ -18,12 +19,7 @@ class _LoginScreenState extends State<LoginScreen> {
         body: Center(
       child: InkWell(
           onTap: () {
-            Navigator.pushNamed(context, RoutesName.home);
-            // Navigator.push(
-            //     context,
-            //     MaterialPageRoute(
-            //       builder: (context) => HomeScreen(),
-            //     ));
+            Utils.toastmessage('No internet connection');
           },
           child: Text('Click')),
     ));
